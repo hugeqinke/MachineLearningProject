db0bf14f4a3832ce5b0c415c80b3bb522b04d778
Thomas Yang <thomas.s.yang@gmail.com>
2016-05-01 11:30:34 -0400

    Reads from api, plus some features that make it easier to use

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
 import os
 import csv
+import sys
 import urllib2
+import StringIO
 
 from bs4 import BeautifulSoup
 
@@ -35,10 +37,10 @@ class Bill(object):
 			if json_data != None:
 				json_data = json_data[path]
                 # Count amendments, actions and cosponsors
-		if attribute == "amendments" or attribute == "actions" or attribute == "cosponsors": 
+		if attribute == "amendments" or attribute == "actions" or attribute == "cosponsors":
 			count = 0
 			for item in json_data:
-				count += 1	
+				count += 1
 			json_data = count
 		if isinstance(json_data, basestring):
 			self.attributes.append(json_data.encode('utf-8').strip())
@@ -75,22 +77,22 @@ class Reader(object):
             bill_t = Bill(open(path, 'r').read(), "JSON")
             bills_t.bills.append(bill_t)
         return bills_t
-    
+
+
     # This method sends a request to GovTrack and retrieves the json info
     # @params:
     # 1. congress_n: the number of congress that we want to get bils for
     # 2. write: specifies whether or not we'd like to write our results to a document
     def scrape_by_congress(self, congress_n, write):
         url = Websites.govtrack_congress_url(congress_n) 
-        page = urllib2.urlopen(url)
-        soup = BeautifulSoup(page, "html.parser")
-    
+        page = urllib2.urlopen(url).read()
+        # Before the write, we need to remove the three line metadata
+        res = {"bills": json.loads(page)["objects"]}
         if write:
-            f = open("./bills" + str(congress_n), "w+")
-            f.write(soup)
+            f = open("./bills/congress" + str(congress_n), "w+")
+            f.write(json.dumps(res, ensure_ascii=True))
             f.close()
-        
-        return soup
+        return res
 
     # List all documents that we want to read and save them to a file,
     # so that we don't have to traverse the file system every time we run the program
@@ -115,8 +117,47 @@ class Algorithms(object):
 
 
 
+# How to use scraping API:
+#   call python main.py scrape
+#   [optional argument after scrape indicating file with desired congress numbers]
+#   default congress number file is ./bills/congress-rules.txt
+#   the scrape call will automatically check the file and obtain any congress number with "true" after it
+#   it will automatically replace it with false after all congress bills have been written
+
 if __name__ == "__main__":
-    r = Reader()
+    cmd = ""
+    if len(sys.argv) > 1:
+        cmd = sys.argv[1]
+
+    # Scrapes in bulk for now
+    if cmd == "scrape":
+        r = Reader()
+        if len(sys.argv) != 3 and len(sys.argv) != 2:
+            print("Usage: python main.py scrape [file with congress numbers and write/not write]")
+
+        if len(sys.argv) == 2:
+            congress_rules = "./bills/congress-rules.txt"
+        else:
+            congress_rules = sys.argv[2]
+
+        f = open(congress_rules, "r+")
+        congresses = f.readlines()
+        successes = ""
+        for congress in congresses:
+            l = congress.split()
+            congress_n = int(l[0])
+            write_indicator = l[1]
+            should_write = (write_indicator.lower() == "true")
+            if r.scrape_by_congress(congress_n, should_write) and should_write:
+                successes += str(congress_n) + " " + str(not should_write).lower() + "\n"
+            if not should_write:  # Already written, should not write again
+                successes += str(congress_n) + " " + str(should_write).lower() + "\n"
+        f.close()
+        f = open(congress_rules, "w+")
+        # automatically rewrite congress rules
+        f.write(successes)
+
+
     # csvfile = open("112data.csv", "wb")
     # w = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     # ATTRIBUTES = [path.strip() for path in open("attributes.txt", "r").readlines()]
@@ -124,4 +165,4 @@ if __name__ == "__main__":
     # bt = r.read_raw_congresses("112json.txt")
     # for bill in bt.bills:
     #    bill.serialize_attributes(w)
-    r.scrape_by_congress(112, True)
+    # r.scrape_by_congress(112, True)
