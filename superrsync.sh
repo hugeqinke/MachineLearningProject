1abb90de245839abe8bc9b9321933468dee7ed78
Thomas Yang <thomas.s.yang@gmail.com>
2016-05-02 22:45:25 -0400

    fixed bash file

--- superrsync.sh
+++ superrsync.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 for i in `seq $1 $2`
 do
-    rsync -avz --exclude "text-versions" --include '*/' --include 'data.json' --delete --delete-excluded --exclude '*' govtrack.us::govtrackdata/congress/$i/bills ./$i    
+    rsync -avz --exclude "text-versions" --include '*/' --include 'data.json' --delete --delete-excluded --exclude '*' govtrack.us::govtrackdata/congress/$i/bills bills/$i    
 done
